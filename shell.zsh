0dad77d472d76de43e1e8d57f0d60b39ceed031f
Will Farris <wfarris@assembledproducts.com>
2020-07-31 10:45:32 -0500

    wttr for weather

--- shell.zsh
+++ shell.zsh
@@ -4,6 +4,7 @@ export VISUAL="nvim"
 alias vim="nvim"
 alias se="cd ~/configurations && nvim shell.zsh"
 alias code="/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin/code"
+alias wttr="wget -qO- wttr.in | head -17"
 
 function configure-up() {
         cd ~/configurations
