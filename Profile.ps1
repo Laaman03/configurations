c231229a2ace143e1314bbdc07ff45a432110691
Will Farris <wfarris@assembledproducts.com>
2020-07-31 16:22:39 -0500

    create Nvim-All function for powershell

--- Profile.ps1
+++ Profile.ps1
@@ -2,3 +2,7 @@ Set-Alias -Name gj -Value Get-Job
 Set-Alias -Name rj -Value Receive-Job
 Set-Alias -Name rmj -Value Remove-Job
 Set-Alias -Name vim -Value nvim
+
+function Nvim-All {
+  Invoke-Expression "nvim -p $((Get-ChildItem -file).Name -join " ")"
+}
